a95f7d115d81d1cb7ea887dc96c8ba5e716e5c98
home <home@DESKTOP-VEKD7C1>
2025-01-24 12:24:15 +0530

    project Added By Saurabh

---
+++ GitHubTask1/src/main/java/com/cjc/GitHubTask1Application.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubTask1Application.class, args);
+	}
+
+}
